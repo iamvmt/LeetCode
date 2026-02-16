b46960bddb914f1c73fa80a869a775309f30d6a5
Vivek Mani Tripathy <vmtprimary@gmail.com>
2026-02-16 15:49:50 +0530

    Time: 2 ms (37.23%) | Memory: 8.5 MB (9.32%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        string str = bitset<32>(n).to_string();
+        reverse(str.begin(), str.end());
+        return stoi(str, 0, 2);
+    }
+};
